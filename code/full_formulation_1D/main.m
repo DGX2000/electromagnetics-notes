6ba426b3104d31503b717b887bae09a27a37fc56
Arne Voss <arnevoss@pm.me>
2021-11-30 13:52:32 +0100

    Initial code for full 1D FDTD simulation

--- code/full_formulation_1D/main.m
+++ code/full_formulation_1D/main.m
@@ -1,29 +1,32 @@
 Z_0 = 376.73;
 epsilon_r = 1;
 mu_r = 1;
+sigma = 1;
 n_grid = 100;
 
 E_x = zeros(1, n_grid);
 H_y = zeros(1, n_grid);
 
+delta_z = 1.0 / n_grid;
+E_x_factor = (Z_0)/(epsilon_r + 0.5*Z_0*sigma*delta_z);
+H_y_factor = (1.0 / (Z_0*mu_r));
 
-timesteps = 200;
+timesteps = 2000;
 for i=1:timesteps
+  E_x(1) = 100.0;
   for j=2:n_grid
-    E_x(j) = E_x(j) - (Z_0 / epsilon_r)*(H_y(j) - H_y(j-1));
+    E_x(j) = E_x(j) - E_x_factor*(H_y(j) - H_y(j-1));
   end
   
-  E_x(n_grid/2) = exp(-0.5 * i);
-  
   for j=1:n_grid-1
-    H_y(j) = H_y(j) - (1.0 / (Z_0*mu_r))*(E_x(j+1) - E_x(j));
+    H_y(j) = H_y(j) - H_y_factor*(E_x(j+1) - E_x(j));
   end
   
   plot3(linspace(0,1,n_grid), zeros(1, n_grid), H_y);
   hold on;
   plot3(linspace(0,1,n_grid), E_x, zeros(1, n_grid));
   hold off;
-  axis([0 1 -1 1 -0.005 0.005]);
+  axis([0 1 -150 150 -1 1]);
   grid on;
   pause(0.025);
 end
